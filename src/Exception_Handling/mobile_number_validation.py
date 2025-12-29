9b20d22705a26d6ad441cbd7594db6f610c5e564
Sowmyasri11 <yenumulasowmya11@gmail.com>
2025-12-29 14:10:59 +0530

    demonstrating custom exceptions

--- src/Exception_Handling/mobile_number_validation.py
+++ src/Exception_Handling/mobile_number_validation.py
@@ -1,8 +1,12 @@
+class InvalidMobileNumberError(Exception):
+    pass
+
 def validate(mobile_number):
     if len(mobile_number) == 10:
         print('Valid mobile number')
     else:
-        raise ValueError
+        raise InvalidMobileNumberError("Enter a proper 10 digit mobile number")
+
 def main():
     mobile_number=input("Enter a mobile number")
     validate(mobile_number)
