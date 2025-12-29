6f05206ba319b7ebe2ae28dc5559c1947e5b959b
Sowmyasri11 <yenumulasowmya11@gmail.com>
2025-12-29 12:19:33 +0530

    handling exception from where the fun is called

--- src/Exception_Handling/exception_handling_mechanism.py
+++ src/Exception_Handling/exception_handling_mechanism.py
@@ -1,19 +1,18 @@
 def fun2():
     print('fun2() started execution')
 
-    try:
-        numerator = int(input('enter a numerator: '))
-        denominator = int(input('enter a denominator: '))
-        result = numerator / denominator
-        print(result)
-    except ZeroDivisionError:
-        print('Exception handles in fun2()')
-
+    numerator = int(input('enter a numerator: '))
+    denominator = int(input('enter a denominator: '))
+    result = numerator / denominator
+    print(result)
     print('fun2() finished execution')
 
 def fun1():
     print('fun1() started execution')
-    fun2()
+    try:
+        fun2()
+    except ZeroDivisionError:
+        print('Exception handled in fun1()')
     print('fun1() finished execution')
 
 def main():
