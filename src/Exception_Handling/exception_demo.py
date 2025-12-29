c86e777d7d4ca5ac087c7e0738aaa6767ae45496
Sowmyasri11 <yenumulasowmya11@gmail.com>
2025-12-29 11:09:17 +0530

    demonstrating else block

--- src/Exception_Handling/exception_demo.py
+++ src/Exception_Handling/exception_demo.py
@@ -4,10 +4,12 @@ try:
     principal_amount = int(input("Enter  principal amount : "))
     time_duration = int(input("Enter  the duration : "))
     rate_of_interest = 10
-    simple_interest = (principal_amount * time_duration * rate_of_interest) / 100
-    print('simple_interest = ',simple_interest)
+
 except:
     print("provide numerical value")
+else:
+    simple_interest = (principal_amount * time_duration * rate_of_interest) / 100
+    print('simple_interest = ', simple_interest)
 
 print("connection has been close to the server")
 
