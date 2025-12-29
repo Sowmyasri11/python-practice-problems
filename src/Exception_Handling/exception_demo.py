6600bcb3982a68143c29e0703d8418e2e51293c9
Sowmyasri11 <yenumulasowmya11@gmail.com>
2025-12-29 10:59:55 +0530

    ADD exception handling using try and except

--- src/Exception_Handling/exception_demo.py
+++ src/Exception_Handling/exception_demo.py
@@ -1,14 +1,13 @@
 print('Secure connection has been established to bank server')
 
-principal_amount=int(input("Enter  principal amount : "))
-
-time_duration=int(input("Enter  the duration : "))
-
-rate_of_interest=10
-
-simple_interest=(principal_amount*time_duration*rate_of_interest)/100
-
-print('simple_interest = ',simple_interest)
+try:
+    principal_amount = int(input("Enter  principal amount : "))
+    time_duration = int(input("Enter  the duration : "))
+    rate_of_interest = 10
+    simple_interest = (principal_amount * time_duration * rate_of_interest) / 100
+    print('simple_interest = ',simple_interest)
+except:
+    print("provide numerical value")
 
 print("connection has been close to the server")
 
