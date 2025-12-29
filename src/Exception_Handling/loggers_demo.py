d0570d1fe63204198213a4e2e8813651d5fbe4c4
Sowmyasri11 <yenumulasowmya11@gmail.com>
2025-12-29 15:23:30 +0530

    creating a log file to keep track the flow of execution

--- src/Exception_Handling/loggers_demo.py
+++ src/Exception_Handling/loggers_demo.py
@@ -1,18 +1,20 @@
+import logging
 def sum_Even(list1):
-    print("sum_even() started execution")
+    logging.info("sum_even() started execution")
 
     sum = 0
     for i in list1:
         if i%2 == 0:
             sum = sum + i
-    print("sum_even() finished execution")
+    logging.info("sum_even() finished execution")
     return sum
 def main():
-    print("main() started execution")
+    logging.basicConfig(filename='log.txt', level=logging.INFO)
+    logging.info("main() started execution")
 
     list1=list(map(int, input().split()))
-    print("input taken from user")
-    print("calling sum_Even()")
+    logging.info("input taken from user")
+    logging.info("calling sum_Even()")
     result = sum_Even(list1)
     print(result)
 
