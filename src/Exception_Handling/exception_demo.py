2b62db384fe291a4c99a4b99a35e9d19aae98e84
Sowmyasri11 <yenumulasowmya11@gmail.com>
2025-12-29 10:45:08 +0530

    ADD exception handling

---
+++ src/Exception_Handling/exception_demo.py
@@ -0,0 +1,16 @@
+print('Secure connection has been established to bank server')
+
+principal_amount=int(input("Enter  principal amount : "))
+
+time_duration=int(input("Enter  the duration : "))
+
+rate_of_interest=10
+
+simple_interest=(principal_amount*time_duration*rate_of_interest)/100
+
+print('simple_interest = ',simple_interest)
+
+print("connection closed")
+
+
+
