d391415f0ad45f1a4c6b9c3da18c2fd287237ba2
Sowmyasri11 <yenumulasowmya11@gmail.com>
2025-12-30 10:52:43 +0530

    dict comprehesion

---
+++ src/Dict/dict_comprehesion.py
@@ -0,0 +1,20 @@
+cities_in_F={'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+
+cities_in_c={key: round((value-32)*(5/9)) for (key, value) in cities_in_F.items()}
+
+print(cities_in_c)
+
+
+# using if condition
+
+weather={'New York': "snowing",'Boston':"sunny",'Los Angeles':"sunny",'Chicago':"cloudy"}
+
+sunny_weather={key: value for (key, value) in weather.items() if value=="sunny"}
+
+print(sunny_weather)
+
+cities={'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+
+desc_cities={key: ("WARM" if value>=40 else "COLD") for (key, value) in cities.items()}
+
+print(desc_cities)
